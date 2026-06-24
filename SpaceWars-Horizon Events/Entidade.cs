8efcffbff68f4699d9ab3e8e7c62cc662b9c16d4
GregoryMinella <miragekun190@gmail.com>
2026-06-24 18:28:43 -0300

    segundo

---
+++ SpaceWars-Horizon Events/Entidade.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceWars_Horizon_Events
+{
+    internal class Entidade
+    {
+    }
+}
